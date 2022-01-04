8dbe70e5ae5539c1832e71df5c3d644834b6064d
kyowankim <kyowangkim@gmail.com>
2022-01-04 17:49:46 -0500

    Fixed profile view and added hover feature

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,5 +7,18 @@ class Comment < ApplicationRecord
     validates_presence_of :post_id
 
     attr_accessor :return_to
+
+    after_create :increment_comment_counter
+    after_destroy :decrement_comment_counter
+
     
+    private
+
+    def increment_comment_counter
+        Post.find(self.post_id).increment(:total_comments_count).save
+    end
+
+    def decrement_comment_counter
+        Post.find(self.post_id).decrement(:total_comments_count).save
+    end
 end
